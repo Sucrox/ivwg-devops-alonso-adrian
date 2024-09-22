e2095d484b47e9e1c6a7a0aa45b93a16c8a7d2a6
Adrian Alonso <Adrianalonso2001@gmail.com>
2024-09-22 10:25:45 +0200

    added search a #7

---
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -0,0 +1,15 @@
+package es.upm.miw.iwvg_devops.code;
+
+import java.util.stream.Stream;
+
+public class Searches {
+
+    public Stream<String> findUserNameBySomeImproperFraction(){
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getFractions().stream()
+                        .filter(Fraction::isImproper)
+                        .count()> 1)
+                .map(User::getName);
+    }
+
+}
---
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/SearchesTest.java
@@ -0,0 +1,18 @@
+package es.upm.miw.iwvg_devops.rest.code;
+
+import es.upm.miw.iwvg_devops.code.Searches;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SearchesTest {
+
+    @Test
+    void testFindUserNameBySomeImproperFraction(){
+        assertEquals(List.of("Ana"), new Searches().findUserNameBySomeImproperFraction()
+              .collect(Collectors.toList()));
+    }
+}
