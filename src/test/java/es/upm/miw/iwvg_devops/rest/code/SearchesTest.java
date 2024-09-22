91d794e4dd31c0969fff7c700a375a604df51da6
Adrian Alonso <Adrianalonso2001@gmail.com>
2024-09-22 11:01:30 +0200

    added search c #10

--- src/test/java/es/upm/miw/iwvg_devops/rest/code/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/SearchesTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.lang.Double.NaN;
+import static java.lang.Double.POSITIVE_INFINITY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
@@ -32,4 +34,10 @@ public class SearchesTest {
     void testFindFirstDecimalFractionByUserNameNull(){
         assertNull(new Searches().findFirstDecimalFractionByUserName("Pedro"));
     }
+    @Test
+    void testFindDecimalFractionByUserName(){
+        assertEquals(List.of(1.0, 1.0, NaN, POSITIVE_INFINITY, 1.0), new Searches().findDecimalFractionByUserName("Paula")
+                .collect(Collectors.toList()));
+    }
+
 }
